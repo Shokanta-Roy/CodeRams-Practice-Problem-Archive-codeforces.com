27b78bbf75694ac8ef2952e4d079f1e8c4364628
Shokanta Roy <141769613+Shokanta-Roy@users.noreply.github.com>
2024-03-19 11:55:10 +0600

    Add files via upload

---
+++ CodeRams Practice Problem Archive/016. Gravity Vehicle Testing.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+
+int main()
+{
+    string ch;
+    cin>>ch;
+    if(ch=="EARTH")
+    {
+        cout<<9.807<<endl;
+    }
+    else if(ch=="MARS")
+    {
+        cout<<3.711<<endl;
+    }
+
+    return 0;
+}
