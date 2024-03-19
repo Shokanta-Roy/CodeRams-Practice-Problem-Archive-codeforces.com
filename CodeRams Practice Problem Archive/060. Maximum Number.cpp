bb3e6338d32a7711b3121d802b891d9376aa8b8a
Shokanta Roy <141769613+Shokanta-Roy@users.noreply.github.com>
2024-03-19 12:15:18 +0600

    Add files via upload

---
+++ CodeRams Practice Problem Archive/060. Maximum Number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+
+int main()
+{
+    long long int n;
+    cin>>n;
+    long long int x= pow(2,n-1)-1;
+    cout<<x<<endl;
+
+    return 0;
+}
+
