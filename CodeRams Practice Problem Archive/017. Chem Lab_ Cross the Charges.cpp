805dff47acdc922a21bb793e075b2b472254463e
Shokanta Roy <141769613+Shokanta-Roy@users.noreply.github.com>
2024-03-19 11:47:39 +0600

    Add files via upload

---
+++ CodeRams Practice Problem Archive/017. Chem Lab_ Cross the Charges.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int c=b,d=a;
+    cout<<c<<" "<<d;
+    return 0;
+}
